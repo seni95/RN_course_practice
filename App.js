59f94e2da12a07e4ff4357c934608dfad6bfaf15
unknown <ghkdtpdus95@naver.com>
2022-12-18 17:48:31 +0900

    initialize assets for version 1

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import AppLoading from "expo-app-loading"
+import { Text,Image } from 'react-native';
+import * as Font from 'expo-font';
+import {Ionicons} from "@expo/vector-icons";
+import {Asset} from 'expo-asset';
+
+const loadFonts = Fonts =>Fonts.map((font)=>Font.loadAsync(font));
+const loadAssets = assets=>assets.map(asset=>{
+  if(typeof asset ==="string")
+  return Image.prefetch(asset);
+  else 
+  return Asset.loadAsync(asset);
+})
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [ready,setReady] = useState(false);
+  const onFinish = ()=>setReady(true);
+  const startLoading = async()=>{
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadAssets([require("./my_man.jpeg"),"https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"])
+    await Promise.all([...fonts,...images]);
+  };
+  if(!ready){
+    return <AppLoading 
+    startAsync={startLoading}
+    onFinish={onFinish} 
+    onError={console.error}></AppLoading>;
+  }
+  return <Text>We are done Loading</Text>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
