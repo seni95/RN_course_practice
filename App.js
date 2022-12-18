0814b9b546838b0401607d5402a7bb6b52827ab8
unknown <ghkdtpdus95@naver.com>
2022-12-18 17:49:07 +0900

    initialize data and assets for version 2

--- App.js
+++ App.js
@@ -3,29 +3,15 @@ import AppLoading from "expo-app-loading"
 import { Text,Image } from 'react-native';
 import * as Font from 'expo-font';
 import {Ionicons} from "@expo/vector-icons";
-import {Asset} from 'expo-asset';
+import {Asset, useAssets} from 'expo-asset';
 
-const loadFonts = Fonts =>Fonts.map((font)=>Font.loadAsync(font));
-const loadAssets = assets=>assets.map(asset=>{
-  if(typeof asset ==="string")
-  return Image.prefetch(asset);
-  else 
-  return Asset.loadAsync(asset);
-})
+
 
 export default function App() {
-  const [ready,setReady] = useState(false);
-  const onFinish = ()=>setReady(true);
-  const startLoading = async()=>{
-    const fonts = loadFonts([Ionicons.font]);
-    const images = loadAssets([require("./my_man.jpeg"),"https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"])
-    await Promise.all([...fonts,...images]);
-  };
-  if(!ready){
-    return <AppLoading 
-    startAsync={startLoading}
-    onFinish={onFinish} 
-    onError={console.error}></AppLoading>;
+  const [assets] = useAssets([require('./my_man.jpeg')])
+  const [loaded,error] = Font.useFonts(Ionicons.font);
+  if(!assets ||!loaded){
+    return <AppLoading></AppLoading>;
   }
   return <Text>We are done Loading</Text>;
 }
