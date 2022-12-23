a9ade68063ccf591ae7ea7e42360b1d723dff6b4
unknown <ghkdtpdus95@naver.com>
2022-12-23 10:19:18 +0900

    before initializing stack navigator

--- navigation/Tab.js
+++ navigation/Tab.js
@@ -4,15 +4,40 @@ import Search from "../screens/Search";
 import Tv from "../screens/Tv";
 import { useColorScheme } from "react-native";
 import { BLACK_COLOR, YELLOW_COLOR } from "../color";
+import {Ionicons} from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = ()=>{
     const isDark = useColorScheme()==="dark";
-    return (<Tab.Navigator screenOptions={{}}>
-    <Tab.Screen name="Movies" component={Movies}/>
-    <Tab.Screen name="Tv" component={Tv}/>
-    <Tab.Screen name="Search" component={Search}/>
+    return (<Tab.Navigator screenOptions={{
+        tabBarStyle:{
+        backgroundColor : isDark?BLACK_COLOR:"white"
+        },
+        tabBarActiveTintColor:isDark?YELLOW_COLOR:BLACK_COLOR,
+        tabBarInactiveTintColor:isDark?"#F6F6F6":"#A6A9B6",
+        headerStyle:{
+            backgroundColor : isDark?BLACK_COLOR:"white"
+        },
+        headerTitleStyle:{
+            color:isDark?YELLOW_COLOR:BLACK_COLOR,
+        }
+    }}>
+    <Tab.Screen name="Movies" component={Movies} options={{
+        tabBarIcon:({focused,color,size})=>{
+            return <Ionicons name="film" size={size} color={color} />
+        }
+    }}/>
+    <Tab.Screen name="Tv" component={Tv} options={{
+        tabBarIcon:({focused,color,size})=>{
+            return <Ionicons name="tv" size={size} color={color} />
+        }
+    }}/>
+    <Tab.Screen name="Search" component={Search} options={{
+        tabBarIcon:({focused,color,size})=>{
+            return <Ionicons name="search" size={size} color={color} />
+        }
+    }}/>
 </Tab.Navigator>)}
 
 export default Tabs;
