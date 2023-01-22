e58eb481cff0e7b41a06b1fdeae625e4a013d94b
unknown <ghkdtpdus95@naver.com>
2023-01-22 22:42:39 +0900

    initialize movie screen for other works

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -3,19 +3,7 @@ import React from 'react';
 import {TouchableOpacity,Text,StyleSheet} from "react-native";
 import styled from "styled-components/native";
 
-const Btn = styled.TouchableOpacity`
-flex:1;
-justify-content:center;
-align-items:center;
-background-color:${props=>props.theme.mainBgColor}
-`;
 
-
-const Title = styled.Text`
-color:${(props)=>props.theme.textColor}
-`; 
-
-const Movies : React.FC<NativeStackScreenProps<any,'Movies'>> = ({navigation:{navigate}})=><Btn 
-onPress={()=>navigate("Stack",{screen:"Three"})}><Title>Movies</Title></Btn>;
+const Movies : React.FC<NativeStackScreenProps<any,'Movies'>> = ()=>null;
 
 export default Movies;
