c3b3479e9b6b834c68520a19c963d0e692c068fb
unknown <ghkdtpdus95@naver.com>
2023-01-23 03:12:26 +0900

    customize swiper

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -4,6 +4,7 @@ import {TouchableOpacity,Text,StyleSheet} from "react-native";
 import styled from "styled-components/native";
 import Swiper from 'react-native-web-swiper';
 // import { View } from 'react-native';
+import { Dimensions } from 'react-native';
 
 const ScrollView = styled.ScrollView`
 backgroundColor : ${(props)=>props.theme.mainBgColor}
@@ -12,8 +13,13 @@ backgroundColor : ${(props)=>props.theme.mainBgColor}
 const View = styled.View`
 flex:1`;
 
+const {height} = Dimensions.get("window");
+
 const Movies : React.FC<NativeStackScreenProps<any,'Movies'>> = ()=><ScrollView>
-    <Swiper containerStyle={{width:"100%", height:300}}>
+    <Swiper loop 
+    timeout={3.5}
+    controlsEnabled={false}
+    containerStyle={{width:"100%", height:height/4}}>
         <View style={{backgroundColor:"red"}}></View>
         <View style={{backgroundColor:"blue"}}></View>
         <View style={{backgroundColor:"red"}}></View>
